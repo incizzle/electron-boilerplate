f237a9eb2f1bffe4ed15a92670f1ffcba13a0781
Mathew <incizzle@gmail.com>
2020-02-07 09:47:26 -0500

    React Dev Tools

--- app/main/electron.ts
+++ app/main/electron.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow, app, ipcMain } from "electron";
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
 const { format } = require('url')
 const { resolve } = require('app-root-path')
@@ -41,4 +42,7 @@ function createWindow(): void {
 
 app.on("ready", () => {
   createWindow();
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then(name => console.log(`Added Extension: ${name}`))
+    .catch(err => console.log("An error occurred: ", err));
 });
